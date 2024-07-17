d95e58e76545ccd04ea72ba3ce10bc5c983720a5
Eyasin Arafath <175514956+Eyasin707@users.noreply.github.com>
2024-07-17 23:11:26 +0600

    Update Rules.py

--- Rules.py
+++ Rules.py
@@ -31,3 +31,21 @@ print(arr3[0,2:])
 print(arr2.dtype)
 new_format=arr2.astype(bool)
 print(new_format)
+
+import numpy as np
+arr1= np.array([1,2,3,4,5,6,7,8,9])
+filt_arr= arr1%2==0
+new_arr=arr1 [filt_arr]
+print(new_arr)
+
+import numpy as np
+arr1= np.array([1,2,3,4,5,6,7,8,9])
+fil_arr=[]
+
+for element in arr1:
+    if element%2==0:
+        fil_arr.append(True)
+    else:
+        fil_arr.append(False)
+new_arrr= arr1[fil_arr]
+print(new_arr)
