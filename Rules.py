cae631cd95a8bfa1b27a44d83908dba7761b64f9
Eyasin Arafath <175514956+Eyasin707@users.noreply.github.com>
2024-07-17 15:55:57 +0600

    Update Rules.py

--- Rules.py
+++ Rules.py
@@ -18,3 +18,16 @@ arr3 = np.array([[1,2,3],[4,5,6],[7,8,9]])
 print(arr1[2])
 print(arr2[0,0])
 print("The value in 3rd row and 3rd collumn:",arr3[2,2])
+
+#slicing:
+arr1 = np.array([1,2,3,4,5])
+print(arr1[:4])
+arr2 = np.array([[1,2,3],[4,5,6]])
+print(arr2[1,:3])
+arr3 = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(arr3[0,2:])
+
+#data type:
+print(arr2.dtype)
+new_format=arr2.astype(bool)
+print(new_format)
